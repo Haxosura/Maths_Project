0c0829e070d86eb23ecc267b2a88c50ae98344ba
Haxosura <123653087+Haxosura@users.noreply.github.com>
2024-04-16 11:54:31 +0100

    Fixing Movement

---
+++ Assets/Scripts/MyMaths.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class MyMaths
+{
+    //Workshop 3
+
+    public static float VectorToRadians(Vector2 V)
+    {
+        float rv = 0.0f;
+
+        rv = Mathf.Atan(V.y / V.x);
+
+        return rv;
+    }
+
+    public static Vector2 RandiansToVector(float Angle)
+    {
+        Vector2 rv = new Vector2(Mathf.Cos(Angle), Mathf.Sin(Angle));
+
+        return rv;
+    }
+
+    public static MyVector RadianstoDegrees(MyVector Radians)
+    {
+        MyVector Degrees = new MyVector(0, 0, 0);
+
+        Degrees.x = Radians.x * 180 / Mathf.PI;
+        Degrees.y = Radians.y * 180 / Mathf.PI;
+        Degrees.z = Radians.z * 180 / Mathf.PI;
+
+        return Degrees;
+    }
+
+    public static MyVector DegreesToRadians(MyVector Degrees)
+    {
+        MyVector Radians = new MyVector(0, 0, 0);
+
+        Radians.x = Degrees.x / (180 / Mathf.PI);
+        Radians.y = Degrees.y / (180 / Mathf.PI);
+        Radians.z = Degrees.z / (180 / Mathf.PI);
+
+        return Radians;
+    }
+
+    public static MyVector EularAngleToDirection(MyVector EulerAngle)
+    {
+        MyVector rv = new MyVector(0, 0, 0);
+
+        rv.z = Mathf.Cos(EulerAngle.y) * Mathf.Cos(-EulerAngle.x);
+        rv.y = Mathf.Sin(-EulerAngle.x);
+        rv.x = Mathf.Cos(-EulerAngle.x) * Mathf.Sin(EulerAngle.y);
+
+        return rv;
+    }
+
+    public static MyVector VectorCrossProduct(MyVector A, MyVector B)
+    {
+        MyVector C = new MyVector(0, 0, 0);
+
+        C.x = A.y * B.z - A.z * B.y;
+        C.y = A.z * B.x - A.x * B.z;
+        C.z = A.x * B.y - A.y * B.x;
+
+        return C;
+    }
+
+    public static Vector3 VectorLerp(Vector3 A, Vector3 B, float T)
+    {
+        return A * (1.0f - T) + B * T;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float MoveSpeed;
-    public float SprintSpeed;
+    MyVector Euler;
+    MyVector EulerDirection;
+    MyVector ForwardDirection;
+
+    MyVector EulerUp;
+    MyVector RightDirection;
+
+    Vector3 EulerVector;
+
+    public float Speed;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        Euler = new MyVector(this.transform.eulerAngles.x, this.transform.eulerAngles.y, this.transform.eulerAngles.z);
+        EulerDirection = MyMaths.DegreesToRadians(Euler);
+        ForwardDirection = MyMaths.EularAngleToDirection(EulerDirection);
 
-        float Speed = Input.GetKey(KeyCode.LeftShift) ? SprintSpeed : MoveSpeed;
+        EulerUp = new MyVector(0, 1, 0);
+        RightDirection = MyMaths.VectorCrossProduct(EulerUp, ForwardDirection);
 
-        transform.position += MoveDirection * Speed * Time.deltaTime;
+        EulerVector = ForwardDirection.ToUnityVector();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += EulerVector * (Time.deltaTime * Speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= EulerVector * (Time.deltaTime * Speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += RightDirection.ToUnityVector() * (Time.deltaTime * Speed);
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= RightDirection.ToUnityVector() * (Time.deltaTime * Speed);
+        }
     }
 }
